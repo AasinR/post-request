e3b2ff54db08896c6f7567bc3633f795114ee296
AasinR <bazsi2001018@gmail.com>
2022-03-14 01:22:14 +0100

    added express-session package

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,36 @@
 import express from "express";
 import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import session from "express-session";
 
 // initialize configuration
 dotenv.config();
 
 const server = express();
-const port = process.env.SERVER_PORT;
+const {
+    NODE_ENV = "development",
+    SERVER_PORT = 8080,
+    SESSION_LIFETIME = 86400000,
+    SESSION_NAME = "sid",
+    SESSION_SECRET = "I/have%no%idea%what%to%put/here"
+} = process.env;
+const inProd = NODE_ENV === "production"
+
+// use bodyParser to read post body
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(express.json());
+
+server.use(session({
+    name: SESSION_NAME,
+    resave: false,
+    saveUninitialized: false,
+    secret: SESSION_SECRET,
+    cookie: {
+        maxAge: 86400000,
+        sameSite: true,
+        secure: inProd
+    }
+}))
 
 // route handler
 server.get("/", (req, res) => {
@@ -14,9 +39,30 @@ server.get("/", (req, res) => {
     });
 })
 
+// test login
+server.post("/testPost", (req, res) => {
+    const { id, name } = req.body;
+    req.session.userId = id;
+    res.json({
+        "id": id,
+        "name": name,
+        "success": true
+    });
+})
+// logout test
+server.post("/testLogout", (req, res) => {
+    req.session.destroy(err => {
+        if(err) {
+            res.status(500).send("Login failed");
+        }
+        res.clearCookie(SESSION_NAME);
+        res.status(200).send("Successful logout");
+    })
+})
+
 
 // start server
-server.listen(port, () => {
+server.listen(SERVER_PORT, () => {
     // tslint:disable-next-line:no-console
-    console.log(`Server started at: http://localhost:${port}`);
+    console.log(`Server started at: http://localhost:${SERVER_PORT}`);
 })

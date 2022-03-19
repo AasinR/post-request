7c3c4dacbfc187056b5e97e5693fb1d6d6d5215a
Virágh Vivien <h053726@stud.u-szeged.hu>
2022-03-19 19:39:37 +0100

    login page, registration page

---
+++ frontend/src/views/Login.vue
@@ -0,0 +1,153 @@
+<template>
+  <div class="login">
+    <WelcomeBar/>
+    <div class="login-form">
+      <div class="login-title">
+        <h1>To use this website, you have to log in first!</h1>
+      </div>
+      <div class="input-fields">
+        <div class="input-group">
+          <label for="email"> E-mail:</label> <br>
+          <div class="input-field">
+            <input type="email"  id="email" v-model="inputData.email">
+          </div>
+        </div>
+        <div class="input-group">
+          <label for="password"> Password:</label> <br>
+          <div class="input-field">
+            <input type="password" id="password" v-model="inputData.password">
+          </div>
+        </div>
+        <div class="login-btn">
+          <button type="submit">Log in</button>
+        </div>
+        <hr>
+        <div class="to-register-btn">
+          <button @click="sendToRegister">Create account</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import WelcomeBar from '@/components/Welcome-Bar'
+
+export default {
+  name: "Login",
+  components: {
+    WelcomeBar
+  },
+
+  data() {
+    return {
+      inputData: {
+        email: '',
+        password: '',
+      },
+    }
+  },
+
+  methods: {
+    sendToRegister(){
+      this.$router.replace({name: 'Register'});
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+
+.login-form {
+  position: absolute;
+  top: 0;
+  right: 0;
+  height: 100vh;
+  width: 55%;
+  background-color: #253f58;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-family: "Cambria",sans-serif;
+
+  .input-fields {
+    width: 40%;
+    border: solid lightblue 2px;
+    border-radius: 30px;
+    padding: 5% 5% 2% 5%;
+    font-size: 1.5rem;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    box-shadow: 10px 10px lightblue;
+
+    .input-group{
+      width: 100%;
+      margin: 5% 0 5% 0;
+      .input-field {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+      }
+      input {
+        width: 95%;
+        font-size: 1.5rem;
+        margin-top: 2%;
+        padding: 3% 0 3% 3%;
+        border: none;
+        border-radius: 20px;
+      }
+    }
+
+    hr {
+      width: 100%;
+    }
+
+    .to-register-btn, .login-btn {
+      width: 100%;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+
+      button {
+        margin-top: 3%;
+        margin-bottom: 5%;
+        font-size: 1.6rem;
+        height: 3.5rem;
+        background-color: #007acc;
+        color: white;
+        border: none;
+        border-radius: 20px;
+        font-weight: bold;
+        font-family: "Cambria",sans-serif;
+        &:hover {
+          cursor: pointer;
+          -webkit-filter: brightness(90%);
+          transition: all 100ms ease;
+        }
+      }
+    }
+
+    .login-btn {
+      button {
+          width: 50%;
+        }
+    }
+
+    .to-register-btn {
+      button {
+        width: 70%;
+      }
+    }
+
+  }
+
+  h1 {
+    font-size: 2.5rem;
+    margin-top: 25%;
+    margin-bottom: 15%
+  }
+
+}
+
+</style>
---
+++ frontend/src/views/Register.vue
@@ -0,0 +1,243 @@
+<template>
+  <div class="register">
+    <WelcomeBar/>
+    <div class="register-form">
+      <div class="register-title">
+        <h1>Registration</h1>
+      </div>
+      <div class="input-fields">
+        <div class="name-group">
+          <div class="input-group firstname">
+            <label for="firstname"> First name:</label> <br>
+            <div class="input-field">
+              <input type="text"  id="firstname" v-model="inputData.firstName">
+            </div>
+          </div>
+          <div class="input-group last-name">
+            <label for="lastname"> Last name:</label> <br>
+            <div class="input-field">
+              <input type="text"  id="lastname" v-model="inputData.lastName" >
+            </div>
+          </div>
+        </div>
+        <div class="input-group">
+          <label for="email"> E-mail:</label> <br>
+          <div class="input-field">
+            <input type="email"  id="email" v-model="inputData.email" >
+          </div>
+        </div>
+        <div class="input-group">
+          <label for="password"> Password:</label> <br>
+          <div class="input-field">
+            <input type="password" id="password" v-model="inputData.password" >
+          </div>
+        </div>
+        <div class="input-group">
+          <label for="password-again"> Password again:</label> <br>
+          <div class="input-field">
+            <input type="password" id="password-again" v-model="inputData.passwordAgain" >
+          </div>
+        </div>
+        <div class="input-group">
+          <label for="phone-number"> Phone number:</label> <br>
+          <div class="input-field">
+            <input type="tel" id="phone-number" v-model="inputData.phoneNumber" >
+          </div>
+        </div>
+        <div class="input-group">
+          <label for="profession"> Current workplace/school:</label> <br>
+          <div class="input-field">
+            <input type="text" id="profession" v-model="inputData.profession">
+          </div>
+        </div>
+        <div class="input-group">
+          <label for="birthdate"> Date of birth:</label> <br>
+          <div class="input-field">
+            <input type="date" id="birthdate" v-model="inputData.birthDate">
+          </div>
+        </div>
+        <div class="input-group">
+          <p>Gender:</p> <br>
+          <div class="input-field radio-field">
+            <input class="radio-input" type="radio" id="male" name="gender" value="male" v-model="inputData.gender">
+            <label for="male">Male</label>
+            <input class="radio-input" type="radio" id="female"  name="gender" value="female" v-model="inputData.gender">
+            <label for="female">Female</label>
+            <input class="radio-input" type="radio" id="custom" name="gender" value="custom" v-model="inputData.gender">
+            <label for="custom">Custom</label>
+          </div>
+        </div>
+        <div class="register-btn">
+          <button type="submit">Sign up</button>
+        </div>
+        <div class="send-to-login">
+          <p>
+            Already have an account?
+            <br>
+            <router-link class="login-link" :to="{name: 'Login'}">Log in here!</router-link>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<script>
+import WelcomeBar from "@/components/Welcome-Bar";
+export default {
+  name: "Register",
+
+  data() {
+    return {
+      inputData: {
+        firstName: '',
+        lastName: '',
+        email: '',
+        password: '',
+        passwordAgain: '',
+        phoneNumber: '',
+        profession: '',
+        birthDate: '',
+        gender: 'male',
+      },
+    }
+  },
+
+  components: {
+    WelcomeBar
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+
+.register-form {
+  position: absolute;
+  top: 0;
+  right: 0;
+  height: 100vh;
+  width: 55%;
+  background-color: #253f58;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-family: "Cambria", sans-serif;
+
+  .input-fields {
+    width: 40%;
+    border: solid lightblue 2px;
+    border-radius: 30px;
+    padding: 3% 5% 2% 5%;
+    font-size: 1.2rem;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    box-shadow: 10px 10px lightblue;
+
+    .input-group {
+      width: 100%;
+      margin: 2% 0 2% 0;
+
+      .input-field {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+
+        &.radio-field{
+          display: flex;
+          flex-direction: row;
+          align-items: center;
+
+          .radio-input {
+            margin: 0 0 0 3%;
+            width: 3rem;
+          }
+        }
+
+        input {
+          width: 95%;
+          font-size: 1rem;
+          margin-top: 2%;
+          padding: 2% 2% 2% 3%;
+          border: none;
+          border-radius: 20px;
+        }
+
+      }
+
+    }
+
+    .send-to-login {
+      p {
+        font-size: 1rem;
+        text-align: center;
+
+        .login-link {
+          color: white;
+        }
+      }
+    }
+
+    p {
+      margin: 0;
+    }
+
+
+    .name-group {
+      width: 100%;
+      .input-group {
+        display: inline-block;
+        width: 48%;
+
+        &.firstname {
+          margin-right: 2%;
+        }
+
+        input {
+          padding: 4% 2% 4% 3%;
+        }
+      }
+    }
+
+    .register-btn {
+      width: 100%;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+
+      button {
+        width: 50%;
+        margin-top: 5%;
+        margin-bottom: 3%;
+        font-size: 1.6rem;
+        height: 3.5rem;
+        background-color: #007acc;
+        color: white;
+        border: none;
+        border-radius: 20px;
+        font-weight: bold;
+        font-family: "Cambria",sans-serif;
+        &:hover {
+          cursor: pointer;
+          -webkit-filter: brightness(90%);
+          transition: all 100ms ease;
+        }
+      }
+    }
+  }
+
+  .register-title{
+    width: 50%;
+    h1 {
+      font-size: 2.5rem;
+      text-align: left;
+      margin-top: 10%;
+      margin-bottom: 5%
+    }
+  }
+
+
+}
+
+</style>

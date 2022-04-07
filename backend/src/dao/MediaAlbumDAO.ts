1854bd0fbed9b9ed51d6dd2d2ece1d76b3066a9c
AasinR <bazsi2001018@gmail.com>
2022-04-08 00:21:35 +0200

    updated album controller

--- backend/src/dao/MediaAlbumDAO.ts
+++ backend/src/dao/MediaAlbumDAO.ts
@@ -21,6 +21,26 @@ class MediaAlbumDAO {
         }
     }
 
+    // get all album by userID
+    async getAll(ID: number): Promise<MediaAlbum[]> {
+        const GET_ALL = `SELECT * FROM mediaalbum WHERE userid = ${ID}`;
+
+        try {
+            const query = await ConnectionConfig.query(GET_ALL);
+            if (query === null) {
+                throw Error("Query failed!");
+            }
+            const result: MediaAlbum[] = [];
+            query.rows.forEach((data: MediaAlbum) => {
+                result.push(data);
+            });
+            return result;
+        } catch(error) {
+            console.error(error);
+            return null;
+        }
+    }
+
     // get album by ID
     async get(ID: number): Promise<MediaAlbum> {
         const GET_ALBUM = `SELECT * FROM mediaalbum WHERE id = ${ID}`;
@@ -39,6 +59,47 @@ class MediaAlbumDAO {
             result.USERID = query.rows[0].USERID;
 
             return result;
+        } catch(error) {
+            console.error(error);
+            return null;
+        }
+    }
+
+    // insert album
+    async create(album: MediaAlbum): Promise<MediaAlbum> {
+        const INSERT_ALBUM = `INSERT INTO mediaalbum (name, userid) VALUES (q'[${album.NAME}]', ${album.USERID}) RETURNING id INTO :id`;
+
+        try {
+            const result = await ConnectionConfig.modify(INSERT_ALBUM, true);
+            album.ID = result;
+
+            return album;
+        } catch(error) {
+            console.error(error);
+            return null;
+        }
+    }
+
+    // update album
+    async update(album: MediaAlbum): Promise<MediaAlbum> {
+        const UPDATE_ALBUM = `UPDATE mediaalbum SET name = q'[${album.NAME}]' WHERE id = ${album.ID}`;
+
+        try {
+            ConnectionConfig.modify(UPDATE_ALBUM, false);
+            return album;
+        } catch(error) {
+            console.error(error);
+            return null;
+        }
+    }
+
+    // delete album
+    async delete(ID: number): Promise<number> {
+        const DELETE_ALBUM = `DELETE FROM mediaalbum WHERE id = ${ID}`;
+
+        try {
+            ConnectionConfig.modify(DELETE_ALBUM, false);
+            return ID;
         } catch(error) {
             console.error(error);
             return null;

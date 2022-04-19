536fea9f0bacd8547ace8cb10a529304533f8b0b
66Gramms <meszarosricsi01@gmail.com>
2022-04-19 17:50:42 +0200

    messaging

--- backend/src/dao/MessageDAO.ts
+++ backend/src/dao/MessageDAO.ts
@@ -19,6 +19,67 @@ class MessageDAO {
             return null;
         }
     }
+
+    async sendMessage(message: Message): Promise<Message> {
+        const SEND = `INSERT INTO privatemessage (Content, TimeStamp, ToUser, FromUser) VALUES ('${message.CONTENT}', ${message.TIMESTAMP}, ${message.TOUSER}, ${message.FROMUSER})`;
+        try {
+            const query = await ConnectionConfig.modify(SEND, true);
+            if (query === null) {
+                throw Error("Query failed!");
+            }
+            return message;
+        } catch(error) {
+            console.error(error);
+            return null;
+        }
+    }
+
+    async getMessage(id: number): Promise<Message> {
+        const FIND_ALL = `SELECT * FROM privatemessage WHERE ID = ${id}`;
+        try {
+            const query = await ConnectionConfig.query(FIND_ALL);
+            if (query === null) {
+                throw Error("Query failed!");
+            }
+            return query.rows[0] as Message;
+        } catch(error) {
+            console.error(error);
+            return null;
+        }
+    }
+
+    async getMessaging(user1: number, user2: number): Promise<Message[]> {
+        const FIND_ALL = `SELECT * FROM privatemessage WHERE FromUser = ${user1} AND ToUser = ${user2}`;
+        try {
+            const query = await ConnectionConfig.query(FIND_ALL);
+            if (query === null) {
+                throw Error("Query failed!");
+            }
+            const result: Message[] = [];
+            query.rows.forEach((data: Message) => {
+                result.push(data);
+            });
+            return result;
+        } catch(error) {
+            console.error(error);
+            return null;
+        }
+    }
+
+    async deleteMessage(id: number, fromUser: number)
+    {
+        const DELETE = `DELETE FROM privatemessage WHERE ID = ${id} AND FromUser = ${fromUser}`;
+        try {
+            const query = await ConnectionConfig.modify(DELETE, true);
+            if (query === null) {
+                throw Error("Query failed!");
+            }
+            return query;
+        } catch(error) {
+            console.error(error);
+            return null;
+        }
+    }
 }
 
 export default new MessageDAO();
--- backend/src/routes/MessageRoutes.ts
+++ backend/src/routes/MessageRoutes.ts
@@ -8,6 +8,11 @@ class MessageRoutes {
     constructor() {
         this._Router = Router();
         this._Router.get("/all", SessionController.isAdmin, MessageController.findAll);
+
+        this._Router.get("/:id", SessionController.isUser, MessageController.getMessage);
+        this._Router.get("/delete", SessionController.isUser, MessageController.deleteMessage);
+        this._Router.get("/get", SessionController.isUser, MessageController.getMessaging);
+        this._Router.post("/send", SessionController.isUser, MessageController.sendMessage);
     }
 
     get Router() {
